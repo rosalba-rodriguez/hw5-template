8d00fb22bc7b01cc720078fba36125bbf6687c20
Rosalba Rodriguez <rosalbaerodriguez@gmail.com>
2016-04-10 21:19:26 -0700

    Update DLinkedStack.h

--- src/DLinkedStack.h
+++ src/DLinkedStack.h
@@ -44,46 +44,98 @@ DLinkedStack<ItemType>::DLinkedStack() : headPtr(nullptr), topPtr(nullptr) {
 template<class ItemType>
 DLinkedStack<ItemType>::DLinkedStack(const DLinkedStack<ItemType> &aStack) {
     //TODO - Implement the copy constructor
+    Node<ItemType> *defaultPtr = headPtr;
+    if (defaultPtr == nullptr)
+        headPtr = nullptr;
+    else {
+        headPtr = new Node<ItemType>();
+        headPtr->setItem(defaultPtr->getItem());
+        Node<ItemType> *newPtr = headPtr;
+        defaultPtr = defaultPtr->getNext();
 
+        while (defaultPtr != nullptr) {
+            ItemType nextItem = defaultPtr->getItem();
+            Node<ItemType> *nodePtr = new Node<ItemType>(nextItem);
+            newPtr->setNext(nodePtr);
+            newPtr = newPtr->getNext();
+            defaultPtr = defaultPtr->getNext();
+        }
+        newPtr->setNext(nullptr);
+    }
 }
 
 template<class ItemType>
 DLinkedStack<ItemType>::~DLinkedStack() {
     //TODO - Implement the destructor
+    Node<ItemType> *deletePtr = headPtr;
+    while (headPtr != nullptr) {
+        headPtr = headPtr->getNext();
+        deletePtr->setNext(nullptr);
+        delete deletePtr;
+        deletePtr = headPtr;
+    }
 }
 
 template<class ItemType>
 Node<ItemType> *DLinkedStack<ItemType>::getPointerTo(const ItemType &target) const {
     //TODO - Return the Node pointer that contains the target(return nullptr if not found)
-
+    Node<ItemType> *newPtr = headPtr;
+    while ((newPtr != nullptr)) {
+        if (target == newPtr->getItem()) {
+            return newPtr;
+        }
+        newPtr = newPtr->getNext();
+    }
     return nullptr;
 }
 
 template<class ItemType>
 bool DLinkedStack<ItemType>::isEmpty() const {
     //TODO - Return True if the list is empty
-
-    return true;
+    return headPtr == nullptr;
 }
 
 template<class ItemType>
 bool DLinkedStack<ItemType>::push(const ItemType &newItem) {
     //TODO - Push an item on the Doubly Linked Stack
-
+    if (isEmpty()) {
+        Node<ItemType> *nodePtr = new Node<ItemType>();
+        nodePtr->setItem(newItem);
+        headPtr = nodePtr;
+        topPtr = nodePtr;
+        return true;
+    }
+    else {
+        Node<ItemType> *nodePtr = new Node<ItemType>();
+        nodePtr->setItem(newItem);
+        topPtr->setNext(nodePtr);
+        nodePtr->setPrev(topPtr);
+        nodePtr->setNext(nullptr);
+        topPtr=nodePtr;
+        return true;
+    }
     return false;
 }
 
 template<class ItemType>
 bool DLinkedStack<ItemType>::pop() {
     //TODO - Pop an item from the stack - Return true if successful
+    Node<ItemType> *newPtr = headPtr;
+    if (isEmpty()) {
+        return false;
 
-    return false;
+    } else{
+        topPtr=topPtr->getPrev();
+        newPtr->setNext(NULL);
+        return true;
+    }
 }
 
 template<class ItemType>
 ItemType DLinkedStack<ItemType>::peek() const {//Assume this never fails.
     //TODO - return the element stored at the top of the stack (topPtr)
-    return 0;//garbage
+    //return 0;//garbage
+    return topPtr->getItem();
 }
 
 template<class ItemType>
